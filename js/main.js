0c83fe25ffe19642428cbf6c455ca45372aaff2b
yuno <dafujunei@gmail.com>
2019-12-06 13:57:04 +0900

    tbodyにタスクを付け足していくように修正。 削除ボタン生成の処理と、作業中/完了ボタン生成の処理は関数化。 キャメルケースで命名。

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
-'use strict'
+'use strict';
 {
-  const table_element = document.getElementById('table_element');
+  const tbodyElement = document.getElementById('tbody_element');
+  const newText = document.querySelector('input[name="new_text"]');
+
   let count = 0;
 
   function btnNum() {
     return count++;
   }
+
+  function createActivityBtn() {  //作業中ボタン作成
+    const stateActivity = document.createElement('input');
+    stateActivity.classList.add('activity');
+    stateActivity.type = 'button';
+    stateActivity.value = '作業中';
+    return stateActivity;
+  }
+
+  function createCompletionBtn() {  //完了ボタン作成
+    const stateCompletion = document.createElement('input');
+    stateCompletion.classList.add('completion');
+    stateCompletion.type = 'button';
+    stateCompletion.value = '完了';
+    return stateCompletion;
+  }
   
+  function createDeleteBtn() {  //削除ボタン作成
+    const stateDeletion = document.createElement('input');
+    stateDeletion.classList.add('deletion');
+    stateDeletion.type = 'button';
+    stateDeletion.value = '削除';
+    return stateDeletion;
+  }
 
+  
   document.getElementById('addition').addEventListener('click',() => {
-    const newtext = document.querySelector('input[name="newtext"]');
-
-    const tr_element = document.createElement('tr');
-    table_element.appendChild(tr_element);
+    const trElement = document.createElement('tr');
+    tbodyElement.appendChild(trElement);
 
     const id = document.createElement('td');
     id.textContent = btnNum();
-    tr_element.appendChild(id);
+    trElement.appendChild(id);
 
     const com = document.createElement('td');
-    com.textContent = newtext.value;
-    tr_element.appendChild(com);
+    com.textContent = newText.value;
+    trElement.appendChild(com);
 
-    const state_activity = document.createElement('input');
-    state_activity.classList.add('activity');
-    state_activity.type = 'button';
-    state_activity.value = '作業中';
-    tr_element.appendChild(state_activity);
+    const a = createActivityBtn();
+    trElement.appendChild(a);
 
-    const state_deletion = document.createElement('input');
-    state_deletion.classList.add('deletion');
-    state_deletion.type = 'button';
-    state_deletion.value = '削除';
-    tr_element.appendChild(state_deletion);
+    const d = createDeleteBtn();
+    trElement.appendChild(d);
   });
 }

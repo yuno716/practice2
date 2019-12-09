2bde448748e17e8dd06614c0058fe80113305f41
yuno <dafujunei@gmail.com>
2019-12-09 16:40:08 +0900

    checkbox 切り替え

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 'use strict';
 {
+  const btn1 = document.querySelector('input[name="btn1"]');
+  const btn2 = document.querySelector('input[name="btn2"]');
+  const btn3 = document.querySelector('input[name="btn3"]');
+
+  function btn1Click() {  //checkbox のすべてクリック時
+    btn1.checked = true;
+    btn2.checked = false;
+    btn3.checked = false;
+  }
+  function btn2Click() {  //checkbox の作業中クリック時
+    btn1.checked = false;
+    btn2.checked = true;
+    btn3.checked = false;
+  }
+  function btn3Click() {  //checkbox の完了クリック時
+    btn1.checked = false;
+    btn2.checked = false;
+    btn3.checked = true;
+  }
+
+  btn1.addEventListener('click', () => {
+    btn1Click();
+  });
+
+  btn2.addEventListener('click', () => {
+    btn2Click();
+  });
+  btn3.addEventListener('click', () => {
+    btn3Click();
+  });
+
+
   const tbodyElement = document.getElementById('tbody_element');
   const newText = document.querySelector('input[name="new_text"]');
 
